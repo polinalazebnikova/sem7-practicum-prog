9e395bd78f4fdaf12bd171be2edc747eb3ef0409
Polina Lazebnikova <54927872+polinalazebnikova@users.noreply.github.com>
2021-11-09 17:31:17 +0800

    Add files via upload

---
+++ ЛР4/WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class WeatherForecastController : Controller
+    {
+        [HttpPost]
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("index")]
+        public async Task<IActionResult> Index()
+        {
+            return this.Json(new { result = "Hello World!" });
+        }
+
+        [HttpPost]
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("hello_world")]
+        public async Task<IActionResult> Test()
+        {
+            MyClassResponse res = new MyClassResponse();
+            res.Success = "success";
+            res.Result = "hello_world!";
+            res.Version = "1.0";
+            return this.Json(res);
+        }
+    }
+}
